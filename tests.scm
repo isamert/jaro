d5fc7ad092db99925d749cdb17ae4cf9e08ebc6d
Isa Mert Gurbuz <isamert@pm.me>
2021-06-30 01:41:45 +0300

    Add bunch of tests and fix some issues

--- tests.scm
+++ tests.scm
@@ -6,12 +6,13 @@
 (define (jaro/clear-state)
   (set! jaro/assocs '())
   (set! jaro/named-assocs (make-hash-table))
-  (set! jaro/cold-run? #t))
+  (set! jaro/cold-run? #f))
 
 (test-begin "all-tests")
 
 (begin
   (jaro/clear-state)
+  (set! jaro/cold-run? #t)
 
   (assoc
    #:pattern ".json$"
@@ -21,6 +22,7 @@
 
 (begin
   (jaro/clear-state)
+  (set! jaro/cold-run? #t)
 
   (assoc
    #:pattern '(".js$" ".json$" ".yaml$")
@@ -28,53 +30,124 @@
 
   (test-equal (jaro/run "~/a.json") "json-viewer ~/a.json"))
 
-(set! jaro/cold-run? #f)
+(jaro/clear-state)
 
 (begin
   (assoc
-   #:pattern "test-on-fail$"
+   #:pattern "test-on-error$"
    #:program "false")
 
-  (test-equal (jaro/run "test-on-fail") 'jaro/non-zero-without-on-error))
+  (test-equal (jaro/run "test-on-error") 'jaro/non-zero-without-on-error))
 
 (begin
   (assoc
-   #:pattern "test-on-fail-2$"
+   #:pattern "test-on-error-2$"
    #:program "false"
    #:on-error "true")
 
-  (test-equal (jaro/run "test-on-fail-2") #t))
+  (test-equal (jaro/run "test-on-error-2") #t))
 
 (begin
   (assoc
-   #:pattern "test-on-fail-3$"
+   #:pattern "test-on-error-3$"
    #:program "false"
    #:on-error "false")
 
-  (test-equal (jaro/run "test-on-fail-3") #f))
+  (test-equal (jaro/run "test-on-error-3") #f))
 
 (begin
   (assoc
-   #:pattern "test-on-fail-4$"
+   #:pattern "test-on-error-4$"
    #:program "false"
    #:on-error 'continue)
 
   (assoc
-   #:pattern "test-on-fail-4$"
+   #:pattern "test-on-error-4$"
    #:program "true")
 
-  (test-equal (jaro/run "test-on-fail-4") #t))
+  (test-equal (jaro/run "test-on-error-4") #t))
 
 (begin
   (assoc
-   #:pattern "test-on-fail-5$"
+   #:pattern "test-on-error-5$"
    #:program "false"
    #:on-error 'continue)
 
   (assoc
-   #:pattern "test-on-fail-5-no-match-on-continue$"
+   #:pattern "test-on-error-5-no-match-on-continue$"
    #:program "true")
 
-  (test-equal (jaro/run "test-on-fail-5") 'jaro/no-matches))
+  (test-equal (jaro/run "test-on-error-5") 'jaro/no-matches))
+
+(begin
+  (assoc
+   #:pattern "test-on-error-6$"
+   #:program "false"
+   #:on-error 'on-error-handler-for-6)
+
+  (assoc
+   #:name 'on-error-handler-for-6
+   #:program "true")
+
+  (test-equal (jaro/run "test-on-error-6") #t))
+
+(begin
+  (set! jaro/cold-run? #t)
+
+  (assoc
+   #:pattern "capture-(\\w+)-test$"
+   #:program "%0 %1 %2")
+
+  (test-equal (jaro/run "capture-group-test") "capture-group-test group %2"))
+
+(begin
+  (set! jaro/cold-run? #t)
+
+  (assoc
+   #:pattern "path-test$"
+   #:program "echo %F")
+
+  (test-equal (jaro/run "full/path-test") (string-append "echo " (getenv "PWD") "/full/path-test")))
+
+(begin
+  (set! jaro/cold-run? #t)
+
+  (assoc
+   #:pattern "path-test$"
+   #:program "echo %F")
+
+  (test-equal (jaro/run "/full/path-test") "echo /full/path-test"))
+
+(begin
+  (set! jaro/cold-run? #t)
+
+  (assoc
+   #:pattern "uri-test$"
+   #:program "echo %U")
+
+  (test-equal (jaro/run "/uri-test") "echo file:///uri-test")
+  (test-equal (jaro/run "https://uri-test") "echo https://uri-test"))
+
+(begin
+  (set! jaro/cold-run? #f)
+
+  (assoc
+   #:pattern "fn-test.(\\w+)$"
+   #:program (lambda (input mimetype matches)
+               (format "~a - ~a - ~a" input mimetype matches)))
+
+  (test-equal (jaro/run "fn-test.mp4") "fn-test.mp4 - video/mp4 - ((%0 . fn-test.mp4) (%1 . mp4))"))
+
+(begin
+  (set! jaro/cold-run? #f)
+
+  (assoc
+   #:pattern "fn-(\\w+)-test$"
+   #:program (lambda (_1 _2 matches) (if (string= (cdr (list-ref matches 1)) "sad")
+                                    #f "SUCCESS"))
+   #:on-error (lambda (_1 _2 _3) "SAD"))
+
+  (test-equal (jaro/run "fn-success-test") "SUCCESS")
+  (test-equal (jaro/run "fn-sad-test") "SAD"))
 
 (test-end "all-tests")
